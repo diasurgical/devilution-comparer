69a49beaa5076378238b0a874c407ab791185dec
Dennis Duda <git@seri.tools>
2018-09-10 21:50:47 +0200

    Added travis and appveyor including pushing releases for tags

---
+++ ci/before_deploy.sh
@@ -0,0 +1,34 @@
+# This script takes care of building your crate and packaging it for release
+
+set -ex
+
+main() {
+    local src=$(pwd) \
+          stage=
+
+    case $TRAVIS_OS_NAME in
+        linux)
+            stage=$(mktemp -d)
+            ;;
+        osx)
+            stage=$(mktemp -d -t tmp)
+            ;;
+    esac
+
+    test -f Cargo.lock || cargo generate-lockfile
+
+    cross rustc --bin devilution-comparer --target $TARGET --release -- -C lto
+
+    cp target/$TARGET/release/devilution-comparer $stage/
+    cp cvdump.exe $stage/
+    cp cvdump-LICENSE $stage/
+    cp comparer-config.toml $stage/
+
+    cd $stage
+    tar cJf $src/$CRATE_NAME-$TRAVIS_TAG-$TARGET.tar.xz *
+    cd $src
+
+    rm -rf $stage
+}
+
+main
